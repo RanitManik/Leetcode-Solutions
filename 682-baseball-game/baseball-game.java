113faae8ef2bd9851b654f65613399d1a3886e78
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-01-10 11:37:56 +0530

    Time: 4 ms (22.38%) | Memory: 41.4 MB (37.34%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,29 +1,21 @@
+import java.util.ArrayList;
+
 class Solution {
     public int calPoints(String[] operations) {
         ArrayList<Integer> numList = new ArrayList<>();
 
-        for (int i = 0; i < operations.length; i++) {
-            try {
-                // If it's a number, parse and add it to the list
-                numList.add(Integer.parseInt(operations[i]));
-            } catch (NumberFormatException e) {
-                // If it's not a number, perform corresponding operation
-                if (operations[i].equals("+")) {
-                    numList.add(numList.get(numList.size() - 1) + numList.get(numList.size() - 2));
-                } else if (operations[i].equals("D")) {
-                    numList.add(numList.get(numList.size() - 1) * 2);
-                } else if (operations[i].equals("C")) {
-                    numList.remove(numList.size() - 1);
-                }
+        for (String op : operations) {
+            if (op.equals("+")) {
+                numList.add(numList.get(numList.size() - 1) + numList.get(numList.size() - 2));
+            } else if (op.equals("D")) {
+                numList.add(numList.get(numList.size() - 1) * 2);
+            } else if (op.equals("C")) {
+                numList.remove(numList.size() - 1);
+            } else {
+                numList.add(Integer.parseInt(op));
             }
         }
 
-        // Calculate the sum of the elements in the list
-        int sum = 0;
-        for (int num : numList) {
-            sum += num;
-        }
-
-        return sum;
+        return numList.stream().mapToInt(Integer::intValue).sum();
     }
 }
