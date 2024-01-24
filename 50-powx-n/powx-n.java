0cb06c2bc45651fef38bace05388fabac7607df0
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-01-24 12:13:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (64.64%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        if (n == 0) {
+            return 1;
+        }
+
+        double temp = myPow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else {
+            if (n > 0) {
+                return x * temp * temp;
+            } else {
+                return (temp * temp) / x;
+            }
+        }
     }
 }
